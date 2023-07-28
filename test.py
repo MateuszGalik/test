74ed5ebd78270ee0a4f7b8058d2d62e2e7d741d7
MateuszGalik <matgal1286@gmail.com>
2023-07-28 22:01:40 +0200

    Prepare train data 80% and test data 20%

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 # PyTorch documentation
 import torch
-print(torch.__version__)
+# print(torch.__version__)
 
 # Setup device agnostic code
 device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using device: {device}")
+# print(f"Using device: {device}")
 
 # Create weight and bias
 weight = 0.7
@@ -22,4 +22,9 @@ X = torch.arange(start, end, step).unsqueeze(dim=1)
 y = weight * X + bias
 # print(f"{X[:5]}, {y[:5]}")
 
-# TODO (create model, and plot)
+# Prepare train data and test data
+train_split = int(0.8 *len(X))  # 80% od data used for training set, 20% for testing
+# print(f"{train_split}")
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+print(f"{len(X_train)}, {len(y_train)}, {len(X_test)}, {len(y_test)}")

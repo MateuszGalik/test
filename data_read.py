eecfd57f4f3f6dd0b657f9b090ddec713fadcf9a
MateuszGalik <matgal1286@gmail.com>
2023-07-29 21:27:29 +0200

    data download and tensor generation

--- data_read.py
+++ data_read.py
@@ -1,3 +1,19 @@
 import pandas as pd
 # print(pd.__version__)
-# TODO import data csv from url
+
+CSV_FILE_LINK = "https://www.multipasko.pl/wyniki-csv.php?f=minilotto-sortowane"
+data = pd.read_csv(CSV_FILE_LINK, delimiter=';')
+# print(data.tail(5))
+data = data.tail(100)
+data_L1 = data.drop(columns=['Numer', 'Miesiac', 'Rok', 'L2', 'L3', 'L4', 'L5'])
+data_L1 = data_L1.rename(columns={'Dzien': 'X', 'L1': 'y'})
+# data_L2 = data.drop(columns=['Numer', 'Miesiac', 'Rok', 'L1', 'L3', 'L4', 'L5'])
+# data_L3 = data.drop(columns=['Numer', 'Miesiac', 'Rok', 'L1', 'L2', 'L4', 'L5'])
+# data_L4 = data.drop(columns=['Numer', 'Miesiac', 'Rok', 'L1', 'L2', 'L3', 'L5'])
+# data_L5 = data.drop(columns=['Numer', 'Miesiac', 'Rok', 'L1', 'L2', 'L3', 'L4'])
+# print(f"{data_L1.tail(5)}")
+data_L1.to_csv('results_L1.csv', index=False, header=False)
+# data_L2.to_csv('results_L2.csv', index=False, header=False)
+# data_L3.to_csv('results_L3.csv', index=False, header=False)
+# data_L4.to_csv('results_L4.csv', index=False, header=False)
+# data_L5.to_csv('results_L5.csv', index=False, header=False)
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 # PyTorch documentation
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
 # print(torch.__version__)
+# print(np.__version__)
 
 # Setup device agnostic code
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -18,13 +21,42 @@ step = 0.02
 # Create X and y (features and labels)
 # without un-squeeze, errors will happen later on (shapes within linear layers)
 X = torch.arange(start, end, step).unsqueeze(dim=1)
-# print(X)
+# X = torch.from_numpy(np.genfromtxt("results_L1.csv", delimiter=",", dtype="int8"))
+# print(X[:5])
 y = weight * X + bias
-# print(f"{X[:5]}, {y[:5]}")
+print(f"{X[:5]}, {y[:5]}")
 
 # Prepare train data and test data
-train_split = int(0.8 *len(X))  # 80% od data used for training set, 20% for testing
-# print(f"{train_split}")
+train_split = int(0.8 * len(X))  # 80% od data used for training set, 20% for testing
+print(f"{train_split}")
 X_train, y_train = X[:train_split], y[:train_split]
 X_test, y_test = X[train_split:], y[train_split:]
 print(f"{len(X_train)}, {len(y_train)}, {len(X_test)}, {len(y_test)}")
+
+
+def plot_predictions(train_data=X_train,
+                     train_labels=y_train,
+                     test_data=X_test,
+                     test_labels=y_test,
+                     predictions=None):
+
+    """
+    Plots training data, test data and compares predictions.
+    """
+    plt.figure(figsize=(10, 7))
+
+    # Plot training data in blue
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+
+    # Plot test data in green
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+    if predictions is not None:
+        # Plot the predictions in red (predictions were made on the test data)
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+    # Show the legend
+    plt.legend(prop={"size": 14})
+
+
+plot_predictions()
